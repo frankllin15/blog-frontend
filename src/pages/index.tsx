bb5dd88f8d453e935e728d190c45302feff811d0
Frankllin <frankllin15@hotmail.com>
2021-09-02 11:28:46 -0300

    Estrutura inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,26 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout home>
+    <>
       <Head>
-        <title>{siteTitle}</title>
+        <title></title>
       </Head>
-     <h1 className="bg-gray-600 text-red-500">Ola</h1>
-    </Layout>
+      <h1 className="bg-gray-600 text-red-500">Ola</h1>
+    </>
   )
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = []
   return {
     props: {
-      allPostsData
-    }
+      allPostsData,
+    },
   }
 }
